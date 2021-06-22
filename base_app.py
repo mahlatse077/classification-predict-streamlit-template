ba58420df34d0a92efe6c2731edcf4bb017aedeb
mahlatse077 <maririasere@gmail.com>
2021-06-22 20:45:54 +0200

    ff
    f

--- base_app.py
+++ base_app.py
@@ -53,7 +53,7 @@ def main():
 	if selection == "Information":
 		st.info("General Information")
 		# You can read a markdown file from supporting resources folder
-		st.markdown("Some information here ,the world is heating up here ")
+		st.markdown("Some information here the world is heating up here ")
 
 		st.subheader("Raw Twitter data and label")
 		if st.checkbox('Show raw data'): # data is hidden if box is unchecked
