14a64235c1a560a8fd0f6318047cd33f10c2916c
mahlatse077 <60735471+mahlatse077@users.noreply.github.com>
2021-06-24 01:26:32 +0200

    justed added now

--- base_app.py
+++ base_app.py
@@ -83,9 +83,9 @@ def main():
 			# Load your .pkl file with the model of your choice + make predictions
 			# Try loading in multiple models to give the user a choice
 			if model_choice=="LogisticRegression":
-                 predictor = joblib.load(open(os.path.join("resources/logreg(1).pkl"),"rb"))
-			     prediction = predictor.predict(vect_text)
-                 st.write(prediction)
+                            predictor = joblib.load(open(os.path.join("resources/logreg(1).pkl"),"rb"))
+			    prediction = predictor.predict(vect_text)
+                            st.write(prediction)
 			# When model has successfully run, will print prediction
 			# You can use a dictionary or similar structure to make this output
 			# more human interpretable.
