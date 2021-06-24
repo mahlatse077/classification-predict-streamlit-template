b1c5a9a22accf34316220080736cf6191c786882
mahlatse077 <maririasere@gmail.com>
2021-06-24 14:23:33 +0200

    ncvn

--- base_app.py
+++ base_app.py
@@ -134,7 +134,7 @@ def main():
 			#docx =nlp(tweet_text)
 			blob = TextBlob(tweet_text)
 			if task_choice =="Tokenization":
-				result =[token for token in blob]
+				result =[token.split() for token in blob]
 				st.json(result)
             
 			#elif task_choice == "Lemmatization":
