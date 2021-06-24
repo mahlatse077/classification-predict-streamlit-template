bd077341bd9b55c2a8bb40468fb03b0e1a68ec5a
mahlatse077 <maririasere@gmail.com>
2021-06-24 09:39:48 +0200

    ff

--- base_app.py
+++ base_app.py
@@ -107,7 +107,7 @@ def main():
 			# When model has successfully run, will print prediction
 			# You can use a dictionary or similar structure to make this output
 			# more human interpretable.
-			st.success("Text Categorized as: {}".format(get_keys(final_result)))
+			st.success("Text Categorized as: {}".format(get_keys(final_result))
 
 # Required to let Streamlit instantiate our web app.  
 if __name__ == '__main__':
